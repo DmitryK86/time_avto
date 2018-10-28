344d8af8b623824347a90ada6598acd551ad1b36
dmitry <d.kurniczkij@cleack.com>
2018-10-28 20:14:19 +0200

    fix table header rendering one more time

--- widgets/views/price.php
+++ widgets/views/price.php
@@ -8,17 +8,18 @@
 <?= \yii\grid\GridView::widget([
     'dataProvider' => $provider,
     'showOnEmpty' => false,
-    'headerRowOptions' => ['style' => 'vertical-align:middle'],
     'layout' => '{items}',
     'columns' => [
         [
             'attribute' => Yii::t('app', 'Наименование работ'),
+            'headerOptions' => ['style' => 'vertical-align:middle'],
             'value' => function($data){
                 return $data['title'];
             }
         ],
         [
             'attribute' => Yii::t('app', 'Стоимость, грн.'),
+            'headerOptions' => ['style' => 'vertical-align:middle'],
             'value' => function($data){
                 return $data['price'];
             }
