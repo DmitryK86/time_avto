3349ba78ebb4f1332ca2fd130f5485e67ad6a321
dmitry <d.kurniczkij@cleack.com>
2018-10-31 20:50:25 +0200

    content fixes

--- views/site/service_pages/insurance.php
+++ views/site/service_pages/insurance.php
@@ -8,7 +8,7 @@
     <div class="decor-h1">
         <h1><?= $model->seo_h1;?></h1>
     </div>
-    <p>В нашем СТО есть офис компании "Super Insurance", осуществляющий все виды автострахования.</p>
+    <p>В нашем <?= Yii::$app->params['appName'];?> есть офис компании "Super Insurance", осуществляющий все виды автострахования.</p>
 
     <div class="section-subs"></div>
 </div>
