9849cceda8b24a6cf503b21cafc0c648db0c2233
dmitry <d.kurniczkij@cleack.com>
2018-11-05 22:02:09 +0200

    add prices to services views on frontend

--- views/site/service_pages/insurance.php
+++ views/site/service_pages/insurance.php
@@ -1,6 +1,7 @@
 <?php
 /**
- *
+ * @var \yii\web\View $this
+ * @var \app\models\ServiceItems $model
  */
 ?>
 
@@ -8,7 +9,13 @@
     <div class="decor-h1">
         <h1><?= $model->seo_h1;?></h1>
     </div>
+
     <p>В нашем <?= Yii::$app->params['appName'];?> есть офис компании "Super Insurance", осуществляющий все виды автострахования.</p>
+
+    <?php if ($model->hasPrice()):?>
+        <h2>Цены на автострахование</h2>
+        <?php echo \app\widgets\Price::widget(['model' => $model]);?>
+    <?php endif;?>
 
     <div class="section-subs"></div>
 </div>
