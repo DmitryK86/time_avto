861e3752e9ae3ea03579c6f28283ef2745bcf111
dmitry <d.kurniczkij@cleack.com>
2018-11-13 21:48:05 +0200

    separate config fixes

--- config/prod/common.php
+++ config/prod/common.php
@@ -1,3 +1,12 @@
 <?php
 
-return [];
+return [
+    'components' => [
+        'db' => [
+            // Schema cache options (for production environment)
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 60,
+            'schemaCache' => 'cache',
+        ],
+    ],
+];
