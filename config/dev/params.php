fe4c9d366be4fbd264486c58ed9735facb3a79a3
dmitry <d.kurniczkij@cleack.com>
2018-11-13 22:03:16 +0200

    fix dev & prod params

--- config/dev/params.php
+++ config/dev/params.php
@@ -4,8 +4,8 @@ return [
     'db' => [
         'host' => (getenv('ENV_MYSQL_HOST') ?: 'localhost') . ':' . (getenv('ENV_MYSQL_PORT') ?:'3306'),
         'dbName' => getenv('MYSQL_DATABASE') ?: 'timeavto',
-        'username' => getenv('ENV_MYSQL_USER') ?: 'admin',
-        'password' => getenv('MYSQL_ROOT_PASSWORD') ?: 'admin',
+        'username' => getenv('ENV_MYSQL_USER') ?: 'root',
+        'password' => getenv('MYSQL_ROOT_PASSWORD') ?: 'root',
         'charset' => 'utf8'
     ]
 ];
