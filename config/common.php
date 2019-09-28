aabf2d8ff0c0c206f2d30333d32d5a65ba29b616
dmitry <kyrnickijdima@gmail.com>
2019-09-28 13:14:14 +0300

    fix common.php

--- config/common.php
+++ config/common.php
@@ -15,7 +15,7 @@ $config = [
             'dsn' => "mysql:host={$params['db']['host']};dbname={$params['db']['dbName']}",
             'username' => "{$params['db']['username']}",
             'password' => "{$params['db']['password']}",
-            'charset' => "{{$params['db']['charset']}}"
+            'charset' => "{$params['db']['charset']}"
         ],
         'user' => [
             'class' => 'yii\web\User',
